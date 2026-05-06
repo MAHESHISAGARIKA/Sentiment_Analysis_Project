6b2bb43c4009c85a7df01d1f2ca6169f08fd9a59
MAHESHISAGARIKA <maheshi.sagarika002@gmail.com>
2026-05-06 07:44:14 +0530

    Improve sentiment model with TF-IDF

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask,render_template,request,redirect
-from helper import preprocessing, vectorizer, get_prediction
+from flask import Flask, render_template, request, redirect
+from helper import preprocessing, get_prediction
 from logger import logging
 
 app = Flask(__name__)
@@ -9,39 +9,40 @@ reviews = []
 positive = 0
 negative = 0
 
+
 @app.route("/")
 def index():
-    data['reviews'] = reviews
-    data['positive'] = positive
-    data['negative'] = negative
+    data["reviews"] = reviews
+    data["positive"] = positive
+    data["negative"] = negative
 
-    logging.info('========== Open home page ============')
-    
-    return render_template('index.html',data = data)
+    logging.info("========== Open home page ============")
 
-@app.route("/", methods = ['post'])
+    return render_template("index.html", data=data)
+
+
+@app.route("/", methods=["POST"])
 def my_post():
-    text = request.form['text']
-    logging.info(f'Text : {text}')
+    global positive, negative
+
+    text = request.form["text"]
+    logging.info(f"Text : {text}")
 
     preprocessed_txt = preprocessing(text)
-    logging.info(f'Preprocessed Text : {preprocessed_txt}')
+    logging.info(f"Preprocessed Text : {preprocessed_txt}")
 
-    vectorized_txt = vectorizer(preprocessed_txt)
-    logging.info(f'Vectorized Text : {vectorized_txt}')
+    prediction = get_prediction(text)
+    logging.info(f"Prediction : {prediction}")
 
-    prediction = get_prediction(vectorized_txt)
-    logging.info(f'Prediction : {prediction}')
-    
-    if prediction == 'negative':
-        global negative
+    if prediction == "negative":
         negative += 1
     else:
-        global positive
         positive += 1
-    
+
     reviews.insert(0, text)
+
     return redirect(request.url)
 
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- helper.py
+++ helper.py
@@ -1,53 +1,32 @@
-import numpy as np
-import pandas as pd
+import pickle
 import re
 import string
-import pickle
 
-from nltk.stem import PorterStemmer
-ps = PorterStemmer()
-
-# load model
-with open('static/model/model.pickle', 'rb') as f:
+# Load trained model
+with open("static/model/model.pickle", "rb") as f:
     model = pickle.load(f)
 
-# load stopwords
-with open('static/model/corpora/stopwords/english', 'r') as file:
-    sw = file.read().splitlines()
+# Load TF-IDF vectorizer
+with open("static/model/vectorizer.pickle", "rb") as f:
+    vectorizer = pickle.load(f)
 
-# load tokens
-vocab = pd.read_csv('static/model/vocabulary.txt', header=None)
-tokens = vocab[0].tolist()
-
-def remove_punctuations(text):
-    for punctuation in string.punctuation:
-        text = text.replace(punctuation, '')
-    return text
 
 def preprocessing(text):
-    data = pd.DataFrame([text], columns=['tweet'])
-    data["tweet"] = data["tweet"].apply(lambda x: " ".join(x.lower() for x in x.split()))
-    data["tweet"] = data['tweet'].apply(lambda x: " ".join(re.sub(r'^https?:\/\/.*[\r\n]*', '', x, flags=re.MULTILINE) for x in x.split()))
-    data["tweet"] = data["tweet"].apply(remove_punctuations)
-    data["tweet"] = data['tweet'].str.replace(r'\d+', '', regex=True)
-    data["tweet"] = data["tweet"].apply(lambda x: " ".join(x for x in x.split() if x not in sw))
-    data["tweet"] = data["tweet"].apply(lambda x: " ".join(ps.stem(x) for x in x.split()))
-    return data["tweet"]
+    text = str(text).lower()
+    text = re.sub(r"http\S+", "", text)
+    text = text.translate(str.maketrans("", "", string.punctuation))
+    text = re.sub(r"\d+", "", text)
+    text = re.sub(r"[^a-z\s]", "", text)
+    text = re.sub(r"\s+", " ", text).strip()
+    return text
 
-def vectorizer(ds):
-    vectorized_lst = []
-    for sentence in ds:
-        sentence_lst = np.zeros(len(tokens))
-        for i in range(len(tokens)):
-            if tokens[i] in sentence.split():
-                sentence_lst[i] = 1  
-        vectorized_lst.append(sentence_lst)
-    vectorized_lst_new = np.asarray(vectorized_lst, dtype=np.float32)
-    return vectorized_lst_new
 
-def get_prediction(vectorized_text):
+def get_prediction(text):
+    cleaned_text = preprocessing(text)
+    vectorized_text = vectorizer.transform([cleaned_text])
     prediction = model.predict(vectorized_text)[0]
+
     if prediction == 1:
-        return 'negative'
+        return "negative"
     else:
-        return 'positive'
+        return "positive"
